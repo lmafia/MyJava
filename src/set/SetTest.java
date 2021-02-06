f13418df3a84c1a739c716138b1f1a7b2eae00d7
lmafia <296427390@qq.com>
2021-02-07 02:14:29 +0800

    HashSet

---
+++ src/set/SetTest.java
@@ -0,0 +1,35 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * @author L_MaFia
+ * @classname SetTest.java
+ * @description TODO
+ * @date 2021/2/7
+ */
+public class SetTest {
+    public static void main(String[] args) {
+        HashSet<String> words = new HashSet<>(16);
+        long totalTime = 0;
+        try (Scanner in = new Scanner(System.in)) {
+            while (in.hasNext()) {
+                String word = in.next();
+                long callTime = System.currentTimeMillis();
+                words.add(word);
+                callTime = System.currentTimeMillis() - callTime;
+                totalTime += callTime;
+            }
+        }
+
+        Iterator<String> iterator = words.iterator();
+        for (int i = 1; i <= 20 && iterator.hasNext(); i++) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("...");
+        System.out.println(words.size() + " distinct words. " + totalTime + "ms");
+    }
+}
